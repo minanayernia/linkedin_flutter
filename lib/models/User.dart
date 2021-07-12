cc8be7b0e511eb5c267ca01645c065197819a238
Mina Nayernia <minanayernia@gmail.com>
2021-07-12 20:49:02 +0430

    get new version

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -4,18 +4,18 @@ import 'package:floor/floor.dart';
 
 @entity
 class  User {
-  @primaryKey
-  final int userId ;
+  @PrimaryKey (autoGenerate: true , ) 
+  int? userId ;
   final int password;
   final String userName ;
 
-  User(this.userId , this.password , this.userName);
+  User( this.password , this.userName);
 }
 
 @dao
 abstract class UserDao {
-  @Query('SELECT * from User where password = :password and userName = :userName ')
-  Stream<User?> findUaerByUsernamePassword(int password , String userName);
+  @Query('SELECT userId FROM User where password = :password and userName = :userName ')
+  Stream<User?> findUserByUsernamePassword(int password , String userName);
 
   @Query('SELECT * FROM User')
   Future<List<User>>findAllusers();
