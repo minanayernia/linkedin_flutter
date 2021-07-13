d797187aa90f25153704db3ceeb9c0bf76650198
Mina Nayernia <minanayernia@gmail.com>
2021-07-13 13:25:18 +0430

    skill and post and like added to database

--- lib/models/Like.dart
+++ lib/models/Like.dart
@@ -1,27 +1,45 @@
+import 'package:dbproject/models/post.dart';
 import 'package:floor/floor.dart';
 
-// CREATE TABLE Likes (
-//     LikeId INT PRIMARY KEY IDENTITY (1, 1),
-//     LikedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-//     UserId INT NOT NULL,
-//     PostId INT NOT NULL,
-//     FOREIGN KEY (UserId) REFERENCES Users (UserId),
-//     FOREIGN KEY (PostId) REFERENCES Posts (PostId)
+import 'User.dart';
 
-// );
+// CREATE TABLE likes (
+//     likeId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+//     likedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+//     userId INT NOT NULL,
+//     postId INT NOT NULL,
+//     FOREIGN KEY (userId) REFERENCES Users (userId),
+//     FOREIGN KEY (postId) REFERENCES Posts (postId)
 
-@entity 
+// )
+
+@Entity(foreignKeys:[
+    ForeignKey(childColumns: ['userId'],
+     parentColumns: ['userId'],
+      entity: User ),
+      ForeignKey(childColumns: ['postId'],
+     parentColumns: ['postId'],
+      entity: Post ),
+] )
 class Like {
-  @primaryKey 
-  final int LikeId ;
-  final DateTime LikedAt ;
-  final int UserId ;
-  final int PostId ;
+  @PrimaryKey  (autoGenerate: true , )
+  int? LikeId ;
+  // final DateTime LikedAt ;
+  @ColumnInfo(name: 'userId')
+  int userId ;
 
-  Like(this.LikeId , this.PostId , this.UserId , this.LikedAt);
+  @ColumnInfo(name: 'postId')
+  int PostId ;
+
+  Like(this.userId , this.PostId);
 }
 
 @dao 
 abstract class LikeDao {
+
+  @Query('SELECT COUNT(likeId) FROM likes WHERE postId = :postId')
+  Future<int> likeNumbers(int postId);
   
+  @insert
+  Future<void> insertLike(Like like);
 }
