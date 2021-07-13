4b3521451897a880f61c85ee938d0d37a5195fb9
Mina Nayernia <minanayernia@gmail.com>
2021-07-13 12:08:33 +0430

    language and accomplishment added to database

--- lib/models/Language.dart
+++ lib/models/Language.dart
@@ -1,20 +1,46 @@
+import 'dart:ffi';
+
 import 'package:floor/floor.dart';
+import 'package:dbproject/models/UserProfile.dart';
 
-// CREATE TABLE Language (
-//     LanguageId INT PRIMARY KEY IDENTITY (1, 1),
-//     Lang VARCHAR (255) NOT NULL,
-//     ProfileId INT NOT NULL,
-//     FOREIGN KEY (ProfileId) REFERENCES UserProfile (ProfileId)
-// );
+// CREATE TABLE languages (
+//     languageId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+//     language VARCHAR (255) NOT NULL,
+//     languageType VARCHAR (255),
+//     profileId INT NOT NULL,
+//     FOREIGN KEY (profileId) REFERENCES userProfile (profileId)
+// )
 
 
-@entity 
+@Entity(foreignKeys:[
+    ForeignKey(childColumns: ['profileId'],
+     parentColumns: ['profileId'],
+      entity: UserProfile ),
+] )
 class Language  {
-  @primaryKey
-  final int LanguageId ;
+  @PrimaryKey  (autoGenerate: true , ) 
+  int? LanguageId ;
 
-  final String Lang  ;
+  @ColumnInfo(name: 'profileId')
+  int? profileId ;
+
+  String languageType;
+  String LanguageName  ;
   final int ProfileId ;
 
-  Language(this.LanguageId , this.Lang , this.ProfileId );
+  Language(this.languageType ,this.LanguageId , this.LanguageName , this.ProfileId );
+}
+
+@dao 
+abstract class LanguageDao {
+
+  @Query('SELECT * FROM Language WHERE profileId = :profileId')
+  Future<List<Language>> allAdditionalInfo(int profileId);
+
+  @Query('UPDATE language SET languageName = :languageName WHERE profileId in (SELECT profileId From userProfile WHERE userId = :userId')
+  Future<Language> editLanguage(String LanguageName , int userId);
+
+  @insert 
+  Future<void> insertLanguage(Language language);
+  
 }
