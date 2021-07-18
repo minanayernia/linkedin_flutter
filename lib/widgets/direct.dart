c366c3d4a4c2a7835cbc67052baa5ce1bbf15297
Mina Nayernia <minanayernia@gmail.com>
2021-07-18 13:34:40 +0430

    a bit

--- lib/widgets/direct.dart
+++ lib/widgets/direct.dart
@@ -42,6 +42,7 @@ void addmsg(var text , var name){
   });
 }
 void conversation(int myid , String othername )async{
+  msg.clear();
   print("we are in conversation!!!");
   widget.db.userDao.findeUserByUserName(othername).then((v) => setState((){
 
